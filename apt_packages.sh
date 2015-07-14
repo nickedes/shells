3ca702a582d0ce77ae9fafe3634d5e0e5189a903
nikhil <nkmittal4994@gmail.com>
2015-07-14 11:27:00 +0530

    Gitignore and todo added for apt-packages! :relaxed:

--- apt_packages.sh
+++ apt_packages.sh
@@ -1,3 +1,5 @@
+# Todo: list all the package in a file with `sudo apt-get install <package-name>`
+
 # Never thought this would so easy! :P
 # http://askubuntu.com/questions/17823/how-to-list-all-installed-packages/17829#17829
 dpkg --get-selections | grep -v deinstall > apt-packages
